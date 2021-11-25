47341748e999b884a419bb42b39ea54394127f12
Pauloricardo2019 <49963863+Pauloricardo2019@users.noreply.github.com>
2021-11-25 19:07:01 -0300

    Add files via upload

---
+++ prototipo site-front/script.js
@@ -0,0 +1,25 @@
+var limpaDados = document.querySelector('input');
+
+function limpar(){
+
+    limpaDados.value = '';
+
+}
+
+limpar();
+
+
+
+function onClickMenu(){
+   
+    document.getElementById('menu').classList.toggle('change2'); 
+    document.getElementById('nav').classList.toggle('change');   
+}
+
+
+
+
+   
+
+
+
